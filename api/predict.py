11b65be81b33623e912c0fc44231f8c0377c6d98
madwin2 <madwin2@gmail.com>
2025-07-29 16:10:11 -0300

    Update predict endpoint: Improved prediction functionality and error handling

--- api/predict.py
+++ api/predict.py
@@ -2,9 +2,19 @@ from http.server import BaseHTTPRequestHandler
 import json
 
 class handler(BaseHTTPRequestHandler):
+    def do_OPTIONS(self):
+        self.send_response(200)
+        self.send_header('Access-Control-Allow-Origin', '*')
+        self.send_header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS')
+        self.send_header('Access-Control-Allow-Headers', '*')
+        self.end_headers()
+        
     def do_POST(self):
         self.send_response(200)
         self.send_header('Content-type', 'application/json')
+        self.send_header('Access-Control-Allow-Origin', '*')
+        self.send_header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS')
+        self.send_header('Access-Control-Allow-Headers', '*')
         self.end_headers()
         
         response = {
